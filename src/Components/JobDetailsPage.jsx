e65bfe2d992266d622ef24045509c8eb40000e93
akibrahman <akibrahman5200@gmail.com>
2023-11-05 19:03:05 +0600

    Apply button condition applied for applying a job

--- src/Components/JobDetailsPage.jsx
+++ src/Components/JobDetailsPage.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { AuthContext } from "./AuthProvider";
 import loader from "/infinite.svg";
 
 const JobDetailsPage = () => {
+  const { user } = useContext(AuthContext);
   const params = useParams();
   const [job, setJob] = useState(null);
   useEffect(() => {
@@ -12,10 +14,19 @@ const JobDetailsPage = () => {
       .get(`http://localhost:5500/job-details/${params.id}`)
       .then((res) => {
         setJob(res.data);
-        console.log(res.data);
       })
       .catch((error) => console.log(error));
   }, [params.id]);
+  const apply = () => {
+    if (user.email === job.authorEmail) {
+      alert("You are the Owner of this Post");
+      return;
+    } else if (!moment().isBefore(job.applicationDeadline)) {
+      alert("Deadline Over");
+      return;
+    }
+    alert("Done");
+  };
   return (
     <div>
       <p>Details</p>
@@ -33,6 +44,14 @@ const JobDetailsPage = () => {
             <span>{job.salaryRangeEnd}</span>
           </p>
           <p>Applicants Number: {job.applicants}</p>
+
+          <button
+            onClick={apply}
+            className="bg-purple-500 font-semibold text-white px-4
+           py-2 rounded-md active:scale-90 duration-300 select-none cursor-pointer"
+          >
+            Apply
+          </button>
         </div>
       ) : (
         <img className="block mx-auto my-20" src={loader}></img>
